c419b8d40fa36f3c060149f4c74d70addb121e2c
CAMARA Mahamadou <doggy_fizzle_11@hotmail.com>
2013-05-19 16:41:07 +0200

    ok

--- src/jms/BrokerLauncher.java
+++ src/jms/BrokerLauncher.java
@@ -1,39 +1,82 @@
 package jms;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.activemq.broker.BrokerFactory;
 import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.broker.TransportConnector;
 import org.apache.log4j.Logger;
 
-public class BrokerLauncher {
-	static BrokerService broker1;
-	static String msgExpected = "Message";
-	static String msgReceived;
-	static Logger logger = Logger.getLogger(BrokerLauncher.class);
+public class BrokerLauncher implements Runnable {
 
-	public static void main(String[] args) {
+	static Logger logger = Logger.getLogger(BrokerLauncher.class);
+	static BrokerLauncher brokerLauncher;
+	
+	BrokerService broker;
+	boolean isFirst = false;
+	Thread self;
+	
+	public boolean isFirst() {
+		return isFirst;
+	}
+
+	public BrokerLauncher()  {
+		self = new Thread(this);
+		self.start();
+		
 		try {
-			setUp();
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			TimeUnit.SECONDS.sleep(5);
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		
+		if(! isFirst){
+			self.interrupt();
+			try {
+				broker.stop();
+			} catch (Exception e) {
+			}
+			System.out.println("second");
+		}else{
+			System.out.println("premier");
+		}
+		
+	}
+	
+	public static BrokerLauncher getBokerLauncher(){
+		if(brokerLauncher == null){
+			brokerLauncher = new BrokerLauncher();
+		}
+		return brokerLauncher;
+	}
+	
+	public static void main(String[] args) {
+
+		BrokerLauncher.getBokerLauncher();
+
 	}
 
-	public static void setUp() throws URISyntaxException, Exception {
-		broker1 = BrokerFactory.createBroker(new URI(
-				"xbean:activemq-static-network-broker1.xml"));
-		TimeUnit.SECONDS.sleep(2);
+	public void stop() throws Exception {
+		broker.stop();
 	}
 
+	@Override
+	public void run() {
+		try {
+			broker = new BrokerService();
 
-	public void tearDown() throws Exception {
-		broker1.stop();
+			TransportConnector connector = new TransportConnector();
+
+			connector.setUri(new URI("tcp://localhost:61616"));
+
+			broker.addConnector(connector);
+
+			broker.start();
+			
+			isFirst = true;
+			
+		}  catch (Exception e) {
+			isFirst = false;
+		}
 	}
 }
